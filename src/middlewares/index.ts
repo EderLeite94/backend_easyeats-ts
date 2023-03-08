0d540770cdb81d803b0290f624100ec2be20399a
Aparecido Leite <eder.aparecidoleite@dxc.com>
2023-03-08 08:17:49 -0300

    fix: cors

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,7 +2,7 @@ const corsOptions = {
     origin: process.env.ORIGIN_CLOUD || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    AccessControlAllowOrigin:['*']
+    accesscontrolalloworigin:['*']
   };
   
   export default corsOptions;
