4d9b2dc842ee568acd0ddeb2057989b6e5a15d83
Aparecido Leite <eder.aparecidoleite@dxc.com>
2023-03-08 08:32:42 -0300

    fix: cors

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,18 @@
-const corsOptions = {
-    origin: process.env.ORIGIN_CLOUD || 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
-  
-  export default corsOptions;
-  
-  
+import cors from 'cors';
+import express, { Application, Request, Response, NextFunction } from 'express';
+const app = express();
+
+const corsOptions = () => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+    //Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X_BPI_CONTEXT');
+    app.use(cors());
+    next();
+  });
+};
+
+export default corsOptions;
+
