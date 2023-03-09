4b3870ae8c7885a6f2f0e9247d39e7baf1459f88
Eder Aparecido Leite <ederaparecidoleite@hotmail.com>
2023-03-09 19:31:19 -0300

    fix: cors

--- dist/middlewares/index.js
+++ dist/middlewares/index.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 function corsMiddleware() {
-    const allowedOrigins = ['http://localhost:3000' || process.env.ORIGIN_CLOUD];
+    const allowedOrigins = ['http://localhost:3000', process.env.ORIGIN_CLOUD];
     const corsOptions = {
         origin: allowedOrigins,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['*'],
         allowedHeaders: ['*'],
     };
     return (0, cors_1.default)();
